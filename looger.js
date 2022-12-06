81202cd0cded3b460c971328986d5c9f19e4eda9
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2022-12-06 16:42:57 +0545

    logger

--- looger.js
+++ looger.js
@@ -7,7 +7,11 @@ const log = (msg) =>{
     const logItem = `\n ${uuid()} \t ${format(new Date(),'yyyy-mm-dd \t hh:mm:ss' ) } \t ${msg}` 
     fs.appendFile(path.join(__dirname,'logs','event-logs.txt'),logItem, (err)=> console.log(err))
 }
+const log2 = (msg) =>{
+    const logItem = `\n ${uuid()} \t ${format(new Date(),'yyyy-mm-dd \t hh:mm:ss' ) } \t ${msg}` 
+    fs.appendFile(path.join(__dirname,'logs','event-logs.txt'),logItem, (err)=> console.log(err))
+}
 
 
-//  pure function is easily to test than impure function
+////  pure function is easily to test than impure function
 module.exports= {log};
