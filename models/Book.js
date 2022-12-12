40f97ffb030a1bfd32d09011179db82fa4968f42
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2022-12-12 16:16:48 +0545

    routes

--- models/Book.js
+++ models/Book.js
@@ -25,6 +25,6 @@ const bookSchema =  mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Category'
     }
-})
+},{timestamps:true})
 
 module.exports = mongoose.model('Book',bookSchema)
--- routes/category-routes.js
+++ routes/category-routes.js
@@ -1,17 +1,18 @@
 const express = require("express")
 const router = express.Router()
+const categorycontroller=require("../contollers/categories_controller")
 
 router.root('/')
-    .get()
-    .post()
+    .get(categorycontroller.getAllCategories)
+    .post(categorycontroller.postnewCategory)
     .put((req,res)=> res.status(501).json({"reply":"Not Implemented"}))
-    .delete()
+    .delete(categorycontroller.deleteCategory)
 
 router.route('/:category_id')
-    .get()
+    .get(categorycontroller.getCategorybyId)
     .post((req,res)=> res.status(501).json({"reply":"Not Implemented"}))
-    .put()
-    .delete()
+    .put(categorycontroller.editCategorybyId)
+    .delete(categorycontroller.deleteCategorybyId)
 
 
 module.exports=router
