f0f6789df4fec51b5be22db0e02930288d2dfcc9
Ashok GC <78782373+Ashok-gc@users.noreply.github.com>
2022-12-14 22:13:53 +0545

    user registration

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,35 +1,39 @@
-const { hash } = require('bcryptjs')
-const express = require('express')
-const User = require('../models/User')
+const express =  require('express')
+const User = require("../models/User")
+const router =  express.Router()
+const bcrypt = require("bcryptjs")
 
-const router = express.Router()
-router.post('/register',(req,res,next)=>{
-    User.findOne({username: req.body.username})
-        .then(user=>{
-            if(user != null){
-                let err=new Error(`User ${req.body.username} already exists`)
-                return next(new Error())
-
-            }
-            bcrypt.hash(req.body.password, 10, (err, hash)=>{
-                if(err) return next(err)
-                let user = new User()
-                user.username = req.body.username
-                user.save().then(user=>{
-                    res.status(201).json({
-                        'status': 'User registered sucessfully',
-                        userId: user._id,
-                        username: user.username
-
+router.post("/register", (req,res,next)=>{
+    User.findOne({username : req.body.username})
+    .then((user)=>{
+        if(user != null){
+            let err =  new Error(`user ${req.body.username} already exists`)
+            return next(err)
+        }
+        else{
+            bcrypt.hash(req.body.password, 10, (err,hash)=>{
+                if(err){
+                    return next(err)
+                }else{
+                    user = new User()
+                    user.username =  req.body.username,
+                    user.password =  hash
+                    user.save().then((user)=>{
+                        res.status(201).json({"reply" : "User Registered Sucessfully",
+                        userId : user._id,
+                        username : user.username
                     })
-                }).catch(next)
+                    })
+                }
             })
-        }).catch(next)
+        }
+    })
+   .catch(next)
+
 })
 
-router.post('/login',(req,res,next)=>{
-    res.send("login required")
+router.post("/login",(req,res,next)=>{
+   
 })
 
-
-module.exports =  router
+module.exports = router
